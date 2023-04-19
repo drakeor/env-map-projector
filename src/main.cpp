d7c0020799b100fe43bc44d061a1cbcbb6aa9a61
drakeor <drakeor.dragon@gmail.com>
2023-04-18 21:48:44 -0700

    updating cmake for release version

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,25 @@ int main()
     ImageWriter writer;
 
 
+
     SkyboxProjection<double> destProj;
-    std::array<EnvMapImage, 6> destImgs = destProj.ConvertToImages(coords.get(), 512);
-    /*
+    std::array<EnvMapImage, 6> destImgs = destProj.ConvertToImages(coords.get(), 2048);
+    
     writer.SaveImage("assets/testoutput/huge_converted_skybox_top.png", destImgs[0]);
     writer.SaveImage("assets/testoutput/huge_converted_skybox_bottom.png", destImgs[1]);
     writer.SaveImage("assets/testoutput/huge_converted_skybox_left.png", destImgs[2]);
     writer.SaveImage("assets/testoutput/huge_converted_skybox_right.png", destImgs[3]);
     writer.SaveImage("assets/testoutput/huge_converted_skybox_front.png", destImgs[4]);
     writer.SaveImage("assets/testoutput/huge_converted_skybox_back.png", destImgs[5]);
-    */
+
+
+
     HemisphericalProjection<double> destProj2;
-    std::array<EnvMapImage, 2> destImgs2 = destProj2.ConvertToImages(coords.get(), 1024);
+    std::array<EnvMapImage, 2> destImgs2 = destProj2.ConvertToImages(coords.get(), 2048);
     
     writer.SaveImage("assets/testoutput/huge_converted_hemi_top.png", destImgs2[0]);
     writer.SaveImage("assets/testoutput/huge_converted_hemi_bottom.png", destImgs2[1]);
+
 
     return 0;
 }
